b18d2ed53ff0102cb89dcbf218a064eae9ebd3bb
tejas.barsawade@gokloud.io <tejas.barsawade@gokloud.io>
2022-01-28 16:48:40 +0530

    myndapp

---
+++ Myend_sampleApp/WebApplication1/Default.aspx.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+
+Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+        Dim conStr As String = ConfigurationManager.ConnectionStrings("conStrLive").ConnectionString
+        Dim con As SqlConnection = New SqlConnection(conStr)
+        Dim oda As SqlDataAdapter = New SqlDataAdapter("SELECT  * from [user]", con)
+        Dim ds As New DataSet()
+        oda.Fill(ds, "data")
+        con.Dispose()
+        oda.Dispose()
+
+
+
+    End Sub
+End Class
